280e666ff480c07ecf217ba74f5bf49340f005ce
Горохов Дмитрий Алексеевич <GorohovD@tsm.local>
2022-02-06 12:30:44 +0300

    Дополнен функционал выводом данных пока работает приложение, переделана архитектура вызова приложения

--- logger.py
+++ logger.py
@@ -1,34 +1,44 @@
 import os
 import psutil
 
-path = psutil.Popen([input("Insert Path: ")]).pid
-#cpuu = psutil.cpu_percent(interval=3)
+
+#path = psutil.Popen([input("Insert Path: ")]).pid
+path = input("Insert Path: ")
+interval = input("Set Interval: ")
+
+def file_Open():
+    file = psutil.Popen([path]).pid
+    return file
+
 
 def cpu_load():
-    process = psutil.Process(path)
+    process = psutil.Process(file)
     cpu_l = process.cpu_percent(interval=1)
     return cpu_l
 
 
 def process_memory():
-    process = psutil.Process(path)
+    process = psutil.Process(file)
     mem_info = process.memory_info()
     return mem_info.rss, mem_info.vms
 
 
 def handles():
-    process = psutil.Process(path)
+    process = psutil.Process(file)
     n_handles = process.num_handles()
     return n_handles
 
+file = file_Open()
 cpu = cpu_load()
 mem = process_memory()
 handl = handles()
 
-print(path)
-print(cpu)
-print(mem)
-print(handl)
+for pid in psutil.pids():
+    while pid == file:
+       print(file)
+       print(cpu)
+       print(mem)
+       print(handl)
 
 
 # C:\Windows\System32\notepad.exe
