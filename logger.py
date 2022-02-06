2268af949351b918e1a1761decea34ecef86a5f7
Горохов Дмитрий Алексеевич <GorohovD@tsm.local>
2022-02-06 20:45:48 +0300

    Промежуточное сохранение

--- logger.py
+++ logger.py
@@ -1,44 +1,53 @@
 import os
 import psutil
+import threading
 
 
-#path = psutil.Popen([input("Insert Path: ")]).pid
 path = input("Insert Path: ")
-interval = input("Set Interval: ")
+interval = int(input("Set Interval: "))
 
-def file_Open():
+
+def proc_Open():
     file = psutil.Popen([path]).pid
     return file
 
 
 def cpu_load():
-    process = psutil.Process(file)
+    process = psutil.Process(proc)
     cpu_l = process.cpu_percent(interval=1)
     return cpu_l
 
 
 def process_memory():
-    process = psutil.Process(file)
+    process = psutil.Process(proc)
     mem_info = process.memory_info()
     return mem_info.rss, mem_info.vms
 
 
 def handles():
-    process = psutil.Process(file)
+    process = psutil.Process(proc)
     n_handles = process.num_handles()
     return n_handles
 
-file = file_Open()
+def log():
+    return proc, cpu, mem, handl
+    #print("Process ID: %i" % proc, "Process used CPU: %i" % cpu, "Process used memory => rss: %i vms: %i" % mem, "Process Handle: %i" % handl)
+
+proc = proc_Open()
 cpu = cpu_load()
 mem = process_memory()
 handl = handles()
+log = str(log())
 
 for pid in psutil.pids():
-    while pid == file:
-       print(file)
-       print(cpu)
-       print(mem)
-       print(handl)
+    while pid == proc:
+        f = open("logger.log", "w")
+        f.write("Process Log %s \n" % path)
+        threading.Timer(interval, f.write(log))
+        print(log)
+        #print(cpu)
+        #print(mem)
+        #print(handl)
 
 
 # C:\Windows\System32\notepad.exe
